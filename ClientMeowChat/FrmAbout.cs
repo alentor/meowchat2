70ec9bd58f521f81e562a643a017a618163d7c70
alen <alen@ALEN-PC>
2020-07-19 20:56:04 +0300

    Initial commit

---
+++ ClientMeowChat/FrmAbout.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace MeowChatClient
+{
+    public partial class FrmAbout : Form
+    {
+        public FrmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/alentor/Moew-Chat");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+MeowChat v4.3
+=======
+School Project, simple chat application using .Net sockets.
+
+You can see the software preview inside the Demo folder
